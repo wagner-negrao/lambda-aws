aea71e6b0a2d93551566cefbbd14a319d202e76c
wagner-negrao <wagnerfelidre+github@gmail.com>
2025-01-15 06:40:32 -0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { log } from './log.mjs';
 
 export const handler = async(event) => {
-
+    log('Variavel de ambiente: ' + process.env.MINHA_VAR)
     log('Log de execução após Github Action. event :' + JSON.stringify(event));
 
     return {
