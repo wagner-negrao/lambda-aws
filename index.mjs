a7ac9c24b4e1e83d5886909abd0985e1ebc7fe38
wagner-negrao <wagnerfelidre+github@gmail.com>
2025-01-15 06:28:33 -0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import { log } from './log.mjs';
 
 export const handler = async(event) => {
 
-    log('Log de execução. event :' + JSON.stringify(event));
+    log('Log de execução após Github Action. event :' + JSON.stringify(event));
 
     return {
         statusCode: 200,
