614eaa6549ee5792c37e721ffc152de067bc800f
wagner-negrao <wagnerfelidre+github@gmail.com>
2025-01-17 06:45:31 -0300

    buscando objetos no S3

--- index.mjs
+++ index.mjs
@@ -1,14 +1,23 @@
 import { log } from './log.mjs';
+import { S3 } from '@aws-sdk/client-s3'
+
+const s3Client = new S3({ region: 'us-east-1' })
 
 export const handler = async(event) => {
-    log('Variavel de ambiente: ' + process.env.MINHA_VAR)
-    log('Log de execução após Github Action. event :' + JSON.stringify(event));
-
-    return {
-        statusCode: 200,
-        body: `<html><body>Dados da requisicao ${JSON.stringify(event)}</body></html>`,
-        headers: {
-            "content-type": "text/html"
-        }
-    };
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    });
+    const mega_byte = 1024 * 1024;
+    
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto muito grande');
+        return 'Objeto muito grande';
+    }
+    
+    log('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
 };
